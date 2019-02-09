66becbd6e6684b8390bd185dd618f26bc9049d01
Tereza Holm <holm.tereza@gmail.com>
2019-02-09 17:07:49 +0100

    Start of project

---
+++ src/se/terhol/entrytest1/Employee.java
@@ -0,0 +1,18 @@
+package se.terhol.entrytest1;
+
+import java.util.List;
+
+/**
+ * @author Tereza Holm
+ */
+public class Employee {
+    private String name;
+    private List<Integer> salary;
+    private int averageSalary;
+
+    public Employee(String name, List<Integer> salary) {
+        this.name = name;
+        this.salary = salary;
+        for (Integer )
+    }
+}
